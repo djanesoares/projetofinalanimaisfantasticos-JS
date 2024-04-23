e6e6c9876f205940def3a1e6e789f2aa2ed5574b
djanesoares <djanesoares@gmail.com>
2024-04-23 09:25:49 -0300

    primeiro commit

---
+++ animais-fantasticos/js/modules/teste-data.js
@@ -0,0 +1,26 @@
+const agora = new Date();
+
+console.log(agora);
+console.log(agora.getDate());
+console.log(agora.getDay());
+console.log(agora.getFullYear());
+console.log(agora.getHours());
+console.log(agora.getMilliseconds());
+console.log(agora.getMinutes());
+console.log(agora.getMonth());
+console.log(agora.getSeconds());
+console.log(agora.getTime());
+console.log(agora.getTimezoneOffset());
+
+// const futuro = new Date('Jun 15 2024 08:59 -3');
+
+// console.log(futuro);
+
+// function transformarDias(tempo) {
+//   return tempo / (24 * 60 * 60 * 1000);
+// }
+
+// const diasAgora = transformarDias(agora.getTime());
+// const diasfuturo = transformarDias(futuro.getTime());
+
+// console.log(diasfuturo - diasAgora);
